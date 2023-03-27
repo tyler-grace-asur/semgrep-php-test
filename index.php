4657247c5aa8bb970f756f925083e26e0be8174a
tyler-grace-asur <122929439+tyler-grace-asur@users.noreply.github.com>
2023-03-27 13:26:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello World1</p>'; ?>
+        <?php echo '<p>Hello World2</p>'; ?>
         <?php echo $_GET[‘id’]; ?>
     </body>
 </html>
